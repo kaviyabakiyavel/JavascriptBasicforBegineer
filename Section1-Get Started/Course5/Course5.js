7a55f669a8290b19cd27f6b928dd1669d086fdd6
B <kaviya.b@senvion.com>
2021-05-04 01:05:04 +0530

    Javascript basic

---
+++ Section1-Get Started/Course5/Course5.js
@@ -0,0 +1,14 @@
+// <!-- seperation of concerns  -->
+
+// <!-- Example thing of your house you have a bed room and your clothes , you dont store clothes in the kitchen .
+// This is what we call seperation of concerns . You have the same principle in programming
+// so we want to seperate html which is all about content , 
+// And javascript is all about behaviour , how should your webpage behave
+// what should happen when we hover our mouse over a given element?
+// Maybe something should pop up maybe somthing should be hidden,
+// -->
+
+
+console.log('Hello World');
+
+document.querySelector('#demo').style.color="green" 
